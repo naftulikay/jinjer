5b8f0029eb3811ceb5e258f7822b41bb8d91e28d
Naftuli Kay <me@naftuli.wtf>
2019-06-21 13:44:59 -0700

    allow specify root level via environment var

--- src/logging.rs
+++ src/logging.rs
@@ -11,13 +11,19 @@ use log4rs::encode::Encode;
 
 use parking_lot::Once;
 
+use std::env;
+
+use std::str::FromStr;
+
 static APPENDER_NAME: &'static str = "stderr";
 
 static INITIALIZE: Once = Once::new();
 
 static LOG_FORMAT: &'static str = "{d(%Y-%m-%dT%H:%M:%S%.3f%z)} {l:5.5} [{T}] {M}: {m}{n}";
 
-static ROOT_LEVEL: LevelFilter = LevelFilter::Error;
+static DEFAULT_ROOT_LEVEL: LevelFilter = LevelFilter::Error;
+
+static ROOT_LEVEL_ENV_VAR: &'static str = "LOG4RS_ROOT_LEVEL";
 
 /// Initialize logging. This function is idempotent.
 pub fn init(config: &Args) {
@@ -45,7 +51,12 @@ fn init_once(config: &Args) {
         Config::builder()
             .appender(Appender::builder().build(APPENDER_NAME, appender))
             .logger(Logger::builder().build("jinjer", level))
-            .build(Root::builder().appender(APPENDER_NAME).build(ROOT_LEVEL))
+            .build(Root::builder().appender(APPENDER_NAME).build(
+                match env::var(ROOT_LEVEL_ENV_VAR) {
+                    Ok(s) => LevelFilter::from_str(s.as_str()).expect("unknown logging level"),
+                    Err(_) => DEFAULT_ROOT_LEVEL,
+                }
+            ))
             .unwrap(),
     )
     .unwrap();
